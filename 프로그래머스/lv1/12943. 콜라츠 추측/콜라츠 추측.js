4ecfa3ee10c806e34caadb291b231501a3c3f9fc
heyimcsy <118671858+heyimcsy@users.noreply.github.com>
2023-02-19 17:03:58 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,46 @@
+function solution(num) {
+    if(num === 1 ){
+        return 0
+    }
+    
+    for(let i = 0; i < 501; i++){
+        if(num == 1){
+            return i
+            break
+        }else if ( i == 500 ){
+            return -1
+        } else if (num % 2 == 0){
+            num = num / 2
+        } else if (num % 2 != 0){
+        num = ( num * 3 ) + 1
+        }
+    }
+}
+//     function solution(num) {
+
+//     if(num === 1){
+//         return 0
+//     }
+        
+        
+//         function solution(num) {
+
+//     if(num === 1){
+//         return 0
+//     }
+
+//     for(let i=0;i<501;i++){
+//         console.log(num,i)
+//         if(num%2===0){
+//             num = num/2
+//         }else if(num%2!==0){
+//             num = (num*3)+1
+//         }else if(num === 1){
+//             return i
+//             break
+//         }else if(i === 500){
+//             return -1
+//         }
+//     }
+// }
+
